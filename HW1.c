c7268aaa52ab17621817c90c8da4a3662dcacc8e
EmaniBoy <karthikemani0321@gmail.com>
2025-02-03 10:23:41 -0600

    updated HW1

--- HW1.c
+++ HW1.c
@@ -4,24 +4,29 @@
 int main(){
 
     //O(n)
-    char name[100];
-    printf("What's your name?\n");
-    scanf("%s", name);
-    printf("Hello, %s!\n", name);
+    int age = 0;
+    printf("How old are you?\n");
+    scanf("%d", age);
+    if(age < 18){
+        printf("You are a minor\n");
+    } else {
+        printf("You are an adult\n");
+    }
+    
 
     //O(n^2)
     int arr[] = {12, 11, 13, 5, 6};
     int totalSum = 0;
-    
-    for(int i = 0; i < sizeof(arr); i++){
-        int currentSum = arr[i] + arr[i+1];
-        for(int j = 0; j < sizeof(arr); j++){
-           totalSum += currentSum;
-           
+    int n = sizeof(arr) / sizeof(arr[0]);  // Number of elements in the array
+
+    for (int i = 0; i < n - 1; i++) {      // Stop at n - 1 to prevent out-of-bounds access
+        int currentSum = arr[i] + arr[i + 1];
+        for (int j = 0; j < n; j++) {      // Loop correctly using the number of elements
+            totalSum += currentSum;
         }
-       
     }
-     printf("Total sum: %d\n", totalSum);
+
+    printf("Total sum: %d\n", totalSum);
 
 
     return 0;
