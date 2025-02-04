75f834545dda2cf604710f48822a0279ae8e67e5
EmaniBoy <karthikemani0321@gmail.com>
2025-02-03 21:00:53 -0600

    changed inner loop index

--- insertionPractice.c
+++ insertionPractice.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void insertion(int arr[], int n){
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i++){
         int key = arr[i];
         int j = i - 1;
         while(j >= 0 && arr[j] > key){
